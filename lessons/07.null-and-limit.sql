ea0fc747053a854fcfa7d93e72d092af509a04e3
ahadsts9901 <ahadsts990@gmail.com>
2024-04-05 11:10:04 +0500

    limit

--- lessons/07.null-and-limit.sql
+++ lessons/07.null-and-limit.sql
@@ -18,3 +18,48 @@ FROM
     orders
 WHERE
     shipped_date IS NULL
+
+
+-- limit clause
+-- usage
+-- -- skip, limit
+-- get first 3 customers
+
+SELECT 
+    *
+FROM
+    customers
+LIMIT 3
+
+
+-- limit clause
+-- skip 3 customers and get next 5 customers
+
+SELECT 
+    *
+FROM
+    customers
+LIMIT 3 , 5
+
+
+-- limit clause
+-- get last 3 customers
+
+SELECT
+    *
+FROM
+    customers
+ORDER BY
+    id DESC
+LIMIT 3
+
+
+-- limit clause
+-- get top 3 loyal customers ( who have more points )
+
+SELECT 
+    *
+FROM
+    customers
+ORDER BY points DESC
+LIMIT 3
