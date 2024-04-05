6b995a636829fbbd35310ab6811a95cb979814bc
ahadsts9901 <ahadsts990@gmail.com>
2024-04-05 10:16:55 +0500

    null-operator

---
+++ lessons/07.null-order-limit.sql
@@ -0,0 +1,23 @@
+-- null opeator
+-- get the customers whom phone number is missing
+
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    phone IS NULL
+
+
+-- null opeator
+-- get all the orders which are not shipped
+
+SELECT 
+    *
+FROM
+    orders
+WHERE
+    shipped_date IS NULL
+
+
+-- order clause
